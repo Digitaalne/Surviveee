622e8bdcbbcd6a0c79d1138f24c77b22a80622d0
jajann <jajann@ttu.ee>
2018-05-14 19:16:56 +0300

    jar

--- Tests/character/CharacterTest.java
+++ Tests/character/CharacterTest.java
@@ -1,6 +1,9 @@
 package character;
+
 import org.junit.jupiter.api.Test;
+import weapons.Bullet;
 import weapons.Weapon;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CharacterTest {
@@ -10,55 +13,80 @@ public class CharacterTest {
         }
 
         @Override
-        public void setWeapon(Weapon weapon)
-        {
+        public void setWeapon(Weapon weapon) {
             if (this.weapon.getGun() == weapon.getGun()) {
                 this.weapon.setBullets(this.weapon.getBullets() + weapon.getBullets());
             } else {
                 this.weapon = weapon;
             }
         }
+        @Override
+        public void bulletZeroCheck() {
+            if (weapon.getBullets() <= 0) {
+                setWeapon(new WeaponTester(Weapon.guns.PISTOL));
+            }
+
+        }
+
+    }
+    public static class WeaponTester extends Weapon{
+        public WeaponTester(guns gun) {
+            this.gun = gun;
+            if (gun.equals(guns.PISTOL)) {
+                bulletType = Bullet.bulletType.SEVEN_MM;
+                bullets = Double.POSITIVE_INFINITY;
+                shootingSpeed = 500;
+            } else if (gun.equals(guns.BOLTACTION)) {
+                bulletType = Bullet.bulletType.WINCHESTER;
+                shootingSpeed = 1250;
+                bullets = 10;
+            } else if (gun.equals(guns.RIFLE)) {
+                bulletType = Bullet.bulletType.NATO;
+                shootingSpeed = 450;
+                bullets = 25;
+            } else if (gun.equals(guns.SUBMACHINE)) {
+                bulletType = Bullet.bulletType.FORTY_FIVE;
+                shootingSpeed = 120;
+                bullets = 150;
+            }
+        }
     }
 
     @Test
     void addingSameWeapon() {
-        CharacterTest1 character = new CharacterTest1(new Weapon(Weapon.guns.RIFLE));
-        character.setWeapon( new Weapon(Weapon.guns.SUBMACHINE));
-        assertEquals(character.getWeapon().getBullets(), 250);
-        character.setWeapon( new Weapon(Weapon.guns.SUBMACHINE));
-        assertEquals(character.getWeapon().getBullets(), 500);
+        CharacterTest1 character = new CharacterTest1(new WeaponTester(Weapon.guns.RIFLE));
+        character.setWeapon(new WeaponTester(Weapon.guns.SUBMACHINE));
+        assertEquals(character.getWeapon().getBullets(), 150);
+        character.setWeapon(new WeaponTester(Weapon.guns.SUBMACHINE));
+        assertEquals(character.getWeapon().getBullets(), 300);
     }
 
     @Test
-    void pickingUpDifferentWeapon()
-    {
-        CharacterTest1 character = new CharacterTest1(new Weapon(Weapon.guns.RIFLE));
-        character.setWeapon( new Weapon(Weapon.guns.SUBMACHINE));
-        assertEquals(character.getWeapon().getBullets(), 250);
+    void pickingUpDifferentWeapon() {
+        CharacterTest1 character = new CharacterTest1(new WeaponTester(Weapon.guns.RIFLE));
+        character.setWeapon(new WeaponTester(Weapon.guns.SUBMACHINE));
+        assertEquals(character.getWeapon().getBullets(), 150);
         assertEquals(character.getWeapon().getGun(), (Weapon.guns.SUBMACHINE));
     }
 
     @Test
-    void loseHealth()
-    {
-        CharacterTest1 character = new CharacterTest1(new Weapon(Weapon.guns.RIFLE));
+    void loseHealth() {
+        CharacterTest1 character = new CharacterTest1(new WeaponTester(Weapon.guns.RIFLE));
         character.setHealth(90);
         assertEquals(character.getHealth(), 90);
     }
 
     @Test
-    void BulletsZeroSwitchToPistol()
-    {
-        CharacterTest1 character = new CharacterTest1(new Weapon(Weapon.guns.RIFLE));
+    void BulletsZeroSwitchToPistol() {
+        CharacterTest1 character = new CharacterTest1(new WeaponTester(Weapon.guns.RIFLE));
         character.getWeapon().setBullets(0);
         character.bulletZeroCheck();
         assertEquals(character.getWeapon().getGun(), Weapon.guns.PISTOL);
     }
 
     @Test
-    void deltaCheck()
-    {
-        CharacterTest1 character = new CharacterTest1(new Weapon(Weapon.guns.RIFLE));
+    void deltaCheck() {
+        CharacterTest1 character = new CharacterTest1(new WeaponTester(Weapon.guns.RIFLE));
         character.setDeltas(1, 1);
         assertEquals(character.getDx(), 1);
         assertEquals(character.getDy(), 1);
--- Tests/weapons/WeaponTest.java
+++ Tests/weapons/WeaponTest.java
@@ -1,33 +1,35 @@
 package weapons;
 
+import character.CharacterTest;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 public class WeaponTest {
     @Test
-    void rightFiringRatio()
-    {
-        Weapon rifle = new Weapon(Weapon.guns.RIFLE);
-        Weapon sub = new Weapon(Weapon.guns.SUBMACHINE);
-        Weapon pistol = new Weapon(Weapon.guns.PISTOL);
-        Weapon bolt = new Weapon(Weapon.guns.BOLTACTION);
+    void rightFiringRatio() {
+        CharacterTest.WeaponTester rifle = new CharacterTest.WeaponTester(Weapon.guns.RIFLE);
+        CharacterTest.WeaponTester sub = new CharacterTest.WeaponTester(Weapon.guns.SUBMACHINE);
+        CharacterTest.WeaponTester pistol = new CharacterTest.WeaponTester(Weapon.guns.PISTOL);
+        CharacterTest.WeaponTester bolt = new CharacterTest.WeaponTester(Weapon.guns.BOLTACTION);
         assertTrue((sub.getShootingSpeed() < pistol.getShootingSpeed() && rifle.getShootingSpeed() < bolt.getShootingSpeed() && rifle.getShootingSpeed() < pistol.getShootingSpeed()));
     }
+
     @Test
-    void setBulletsTest()
-    {
-        Weapon rifle = new Weapon(Weapon.guns.RIFLE);
+    void setBulletsTest() {
+        CharacterTest.WeaponTester rifle = new CharacterTest.WeaponTester(Weapon.guns.RIFLE);
         rifle.setBullets(1);
-        assertEquals(rifle.getBullets(),1);
+        assertEquals(rifle.getBullets(), 1);
         rifle.setBullets(rifle.getBullets() + 1);
         assertEquals(rifle.getBullets(), 2);
     }
+
     @Test
-    void rightEnums()
-    {
-        Weapon rifle = new Weapon(Weapon.guns.RIFLE);
-        Weapon sub = new Weapon(Weapon.guns.SUBMACHINE);
-        Weapon pistol = new Weapon(Weapon.guns.PISTOL);
-        Weapon bolt = new Weapon(Weapon.guns.BOLTACTION);
+    void rightEnums() {
+        CharacterTest.WeaponTester rifle = new CharacterTest.WeaponTester(Weapon.guns.RIFLE);
+        CharacterTest.WeaponTester sub = new CharacterTest.WeaponTester(Weapon.guns.SUBMACHINE);
+        CharacterTest.WeaponTester pistol = new CharacterTest.WeaponTester(Weapon.guns.PISTOL);
+        CharacterTest.WeaponTester bolt = new CharacterTest.WeaponTester(Weapon.guns.BOLTACTION);
         assertEquals(rifle.getGun(), Weapon.guns.RIFLE);
         assertEquals(sub.getGun(), Weapon.guns.SUBMACHINE);
         assertEquals(pistol.getGun(), Weapon.guns.PISTOL);
